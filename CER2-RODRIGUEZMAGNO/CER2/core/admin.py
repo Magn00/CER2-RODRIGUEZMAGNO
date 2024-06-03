be3a8ee42959a9c6db51d9fc165a3abd461f2bcc
Magno Isaac Rodríguez Cubillos <magno.rodriguez.cubillos@gmail.com>
2024-06-03 10:23:50 -0400

    Delete CER2-RODRIGUEZMAGNO/CER2 directory

--- CER2-RODRIGUEZMAGNO/CER2/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Estudiante, Profesor, Proyecto
-
-admin.site.register(Estudiante)
-admin.site.register(Profesor)
-admin.site.register(Proyecto)  
- 
--- CER2-RODRIGUEZMAGNO/CER2/core/forms.py
+++
@@ -1,9 +0,0 @@
-# core/forms.py
-
-from django import forms
-from .models import Proyecto
-
-class ProyectoForm(forms.ModelForm):
-    class Meta:
-        model = Proyecto
-        fields = ['titulo', 'descripcion', 'tematica']
